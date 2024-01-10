976be8c12007f008a547fccb66a85562991731f5
amin-s81 <amin.sanei81@gmail.com>
2024-01-10 19:11:41 +0330

    add final contract codes

--- Context/constants.js
+++ Context/constants.js
@@ -1,5 +1,5 @@
-//TheBlockchainCoders = 0x5FbDB2315678afecb367f032d93F642f64180aa3
-//TokenSale = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+//TheBlockchainCoders = 0x...
+//TokenSale = 0x...
 
 //Json Files
 import TheBlockchainCoders from './TheBlockchainCoders.json';
