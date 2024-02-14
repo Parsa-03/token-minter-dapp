43de954b5c06f9ce5cb454ed6a43cdba2787e1bc
Parsa-03 <parsamaster40@gmail.com>
2024-02-14 14:12:50 +0330

    imported components to index.js in pages folder

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,53 @@ import React from "react";
 
 //internal import
 import { useStateContext } from "../Context/index";
+import {
+  About,
+  ArrowUp,
+  Banner,
+  Blog,
+  Client,
+  Contact,
+  Distribution,
+  Faq,
+  Footer,
+  Header,
+  Loader,
+  MobileApp,
+  Service,
+  Team,
+  TimeLine,
+  TokenSale,
+} from "../Components/index";
 
 const index = () => {
-  const { TOKEN_ICO, transferNativeToken } = useStateContext();
+  const { 
+    transferNativeToken,
+    buyToken,
+    ConnectWallet, 
+    setAddress,
+    TOKEN_ICO,
+    currentHolders,
+    tokenSale,
+    tokenHolders,
+    nativeToken,
+    balance,
+    address,
+   } = useStateContext();
   return (
-    <div>
-      <h1> {TOKEN_ICO} </h1>
-      <button onClick={() => transferNativeToken()}> TRANSFER </button>
+    <div className="v_dark">
+      <Header address={address} setAddress={setAddress} ConnectWallet={ConnectWallet}/>
+      <Banner transferNativeToken = {transferNativeToken} />
+      <Service/>
+      <About/>
+      <tokenSale buyToken={buyToken} tokenSale={tokenSale}/>
+      <Distribution/>
+      <MobileApp/>
+      <Team/>
+      <Faq/>
+      <Contact/>
+      <Footer/>
+
     </div>
   );
 };
