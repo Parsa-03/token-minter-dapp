0c99a78bebfa5dad2564ba19ad57839548188450
Parsa-03 <parsamaster40@gmail.com>
2024-02-16 21:32:28 +0330

    modified constants.js and index.js to test the ICO Dapp on localhost

--- Context/constants.js
+++ Context/constants.js
@@ -6,9 +6,9 @@ import TheBlockchainCoders from './TheBlockchainCoders.json';
 import TokenSale from './TokenSale.json';
 
 //TOKEN
-export const TOKEN_ADDRESS = "0x0";
+export const TOKEN_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 export const TOKEN_ABI = TheBlockchainCoders.abi;
 
 //TOKEN SALE
-export const TOKEN_SALE_ADDRESS= "0x0";
+export const TOKEN_SALE_ADDRESS= "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 export const TOKEN_SALE_ABI = TokenSale.abi;
